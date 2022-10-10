3b07224dcc3b4792f6efd1df18a83b23502953c2
Fateme Oodar <105479013+FatemeOodar1996@users.noreply.github.com>
2022-10-10 17:35:49 +0330

    bugs

---
+++ assets/js/app.js
@@ -0,0 +1,162 @@
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    effect: "cards",
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+/* particles.json... */
+
+particlesJS(
+    "particles-js",
+
+    {
+        particles: {
+            number: {
+                value: 60,
+                density: {
+                    enable: true,
+                    value_area: 800,
+                },
+            },
+            color: {
+                value: "#08fdd8",
+                opacity: 0.3,
+            },
+            shape: {
+                type: "circle",
+                stroke: {
+                    width: 0,
+                    color: "#000000",
+                },
+                polygon: {
+                    nb_sides: 5,
+                },
+                image: {
+                    src: "img/github.svg",
+                    width: 100,
+                    height: 100,
+                },
+            },
+            opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                    enable: false,
+                    speed: 1,
+                    opacity_min: 0.1,
+                    sync: false,
+                },
+            },
+            size: {
+                value: 4,
+                random: true,
+                anim: {
+                    enable: false,
+                    speed: 40,
+                    size_min: 0.1,
+                    sync: false,
+                },
+            },
+            line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#fd1056",
+                opacity: 0.4,
+                width: 1,
+            },
+            move: {
+                enable: true,
+                speed: 6,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                attract: {
+                    enable: false,
+                    rotateX: 600,
+                    rotateY: 1200,
+                },
+            },
+        },
+        interactivity: {
+            detect_on: "canvas",
+            events: {
+                onhover: {
+                    enable: true,
+                    mode: "grab",
+                },
+                onclick: {
+                    enable: true,
+                    mode: "push",
+                },
+                resize: true,
+            },
+            modes: {
+                grab: {
+                    distance: 200,
+                    line_linked: {
+                        opacity: 0.4,
+                    },
+                },
+                bubble: {
+                    distance: 1000,
+                    size: 100,
+                    duration: 2,
+                    opacity: 8,
+                    speed: 3,
+                },
+                repulse: {
+                    distance: 200,
+                },
+                push: {
+                    particles_nb: 4,
+                },
+                remove: {
+                    particles_nb: 2,
+                },
+            },
+        },
+        retina_detect: true,
+        config_demo: {
+            hide_card: false,
+            background_color: "#b61924",
+            background_image: "",
+            background_position: "50% 50%",
+            background_repeat: "no-repeat",
+            background_size: "cover",
+        },
+    }
+);
+
+var swiper = new Swiper(".graphicSwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    loop: true,
+    speed: 800,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
